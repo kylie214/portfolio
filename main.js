7f4a63435a6b22b293627896fdcbfcda0f07a7f2
kylie214 <soohyun1589@naver.com>
2021-07-27 17:16:59 +0900

    JS : Navbar menu toggle + project ui

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ document.addEventListener('scroll', () => {
    }
 });
 
+
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -23,9 +25,15 @@ navbarMenu.addEventListener('click', (event) => {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
 
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+    navbarMenu.classList.toggle('open');
+});
 
 //Handle click on 'contact me' button on home
 const homeContactBtn = document.querySelector('.home__contact');
